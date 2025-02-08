480879708a43318c7a151ad28fb6b20031ff15e2
vansh <vanshpatel1299@gmail.com>
2025-02-09 02:27:59 +0530

    Auth Server

--- backend/auth/src/index.ts
+++ backend/auth/src/index.ts
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/auth", authRoutes);
 
 app.get("/auth", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+    res.send("Auth Server");
 });
 
 app.get('/auth/health', (req: Request, res: Response) => {
